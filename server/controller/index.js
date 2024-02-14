0b201b8e7bb9c7ff45f513bee7990cf144173442
= <barkatullahkhan120136@gmail.com>
2024-02-14 07:30:24 -0800

    password bcrypt

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,15 +1,20 @@
 const Users = require("../models/UserMOdel");
+const bcrypt = require("bcrypt");
 
 const Home = (req, res) => {
   res.send("we are on home page ");
 };
 const register = async (req, res) => {
   const { name, email, phone, password } = req.body;
+  console.log(" requested data", req.body);
   const emailExists = await Users.findOne({ email });
   if (emailExists) {
     return res.status(400).send("user already exist ");
   }
-  await Users.create({ name, email, phone, password });
+  const SaltRound = 10;
+  const hashed_password = await bcrypt.hash(password, SaltRound);
+
+  await Users.create({ name, email, phone, password: hashed_password });
   res.send({ message: req.body });
 };
 
