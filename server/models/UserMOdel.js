9535c5c717f46e61dc866857b3bdd0898fd91eef
= <barkatullahkhan120136@gmail.com>
2024-02-14 05:39:01 -0800

    schema created for registration form

---
+++ server/models/UserMOdel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Users = new mongoose.model("User", userSchema);
+
+module.exports = Users;
