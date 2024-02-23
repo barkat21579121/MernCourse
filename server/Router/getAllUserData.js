f5fef5f8e59d549779f6e2254faf405b84b09cb8
= <barkatullahkhan120136@gmail.com>
2024-02-23 09:36:35 -0800

    admin middleware added

--- server/Router/getAllUserData.js
+++ server/Router/getAllUserData.js
@@ -6,10 +6,11 @@ const {
 } = require("../controller/getAllUserDatacontroller");
 const UserAuthMiddleWare = require("../middleware-Validator/AuthMiddleWare");
 const { deleteUser } = require("../controller");
+const adminMiddleware = require("../middleware-Validator/AuthMiddleWare");
 
 const router = express.Router();
 
-router.get("/users", UserAuthMiddleWare, getAllUserData);
+router.get("/users", adminMiddleware, getAllUserData);
 router.get("/users/:id", UserAuthMiddleWare, getSingleUserData);
 
 router.delete("/users/delete/:id", UserAuthMiddleWare, deleteUser);
--- server/middleware-Validator/AuthMiddleWare.js
+++ server/middleware-Validator/AuthMiddleWare.js
@@ -18,5 +18,30 @@ const UserAuthMiddleWare = async (req, res, next) => {
     next();
   } catch (error) {}
 };
+const adminMiddleware = async (req, res, next) => {
+  const token = await req.header("Authorization");
+  if (!token) {
+    res.status(400).json({ message: "invalid token" });
+    return;
+  }
+
+  const jsonwebtoken = token.replace("Bearer", "").trim();
+  try {
+    const IsVerified = JWT.verify(jsonwebtoken, process.env.JWT_KEY);
+    const UserData = await Users.findOne({ email: IsVerified.email }).select({
+      isAdmin: 1,
+    });
+
+    if (UserData && UserData.isAdmin) {
+      next(); // Admin is verified, access granted!
+    } else {
+      res.status(403).json({ message: "Access Denied! Only admins allowed." });
+    }
+  } catch (error) {
+    console.error("Error in admin middleware:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
 module.exports = UserAuthMiddleWare;
+module.exports = adminMiddleware;
